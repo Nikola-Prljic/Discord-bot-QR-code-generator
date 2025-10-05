4a720d0eb6c033974d0656c780b75123cbce7c2a
nik507 <nik.7377@gmail.com>
2025-10-05 20:23:19 +0200

    style changes move stuff to new file

--- bot.py
+++ bot.py
@@ -1,33 +1,33 @@
-import discord
 import os
-import random
 import qrcode
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+    
+def create_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+    intents.presences = True
+    return commands.Bot(command_prefix="!", intents=intents)
 
-load_dotenv()
+def create_qr_code_img(data):
+    
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L, # pyright: ignore[reportAttributeAccessIssue]
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
 
+    return qr.make_image(fill_color="black", back_color="white")
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-token = os.getenv('TOKEN')
-""" if token is None:
-    return """
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"✅ Logged in as {bot.user}")
-
-""" 
-@bot.command()
-async def qr(ctx, *, text: str):
-    img = qrcode.make(text)
-    img.save("qr.png")
-    await ctx.send(file=discord.File("qr.png")) """
-
-bot.run(token)
+def get_token():
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    if token is None:
+        print("[Error] Token is missing")
+        return ""
+    return token
